3c47333782869c49cae151413178446aac4c138f
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-07-12 15:43:59 -0700

    Add empty Storybook
    + upgrade vite -> 4.4.1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,25 @@
 
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
-  ],
+  env: {
+    browser: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: true,
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
   plugins: ['react-refresh'],
-  ignorePatterns: ['src/components/ui/**'], // ignore shadcn ui components for now since they're unmodified
+  ignorePatterns: ['src/components/ui/**'],
+  // ignore shadcn ui components for now since they're unmodified
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-}
+    'react-refresh/only-export-components': ['warn', {
+      allowConstantExport: true
+    }],
+    '@typescript-eslint/no-non-null-assertion': 'off'
+  }
+};
